80ab0df89f96175937f5045551e1b90dcc61a1f9
1p4pk <leonpapke@gmx.de>
2019-11-12 15:09:45 +0100

    Updated akka version.

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -25,8 +25,8 @@ import akka.actor.ActorSelection;
 import akka.actor.Props;
 import akka.NotUsed;
 import akka.util.ByteString;
-import akka.stream.ActorMaterializer;
 import akka.stream.IOResult;
+import akka.stream.Materializer;
 import akka.stream.SourceRef;
 import akka.stream.javadsl.StreamConverters;
 import akka.stream.javadsl.Sink;
@@ -64,7 +64,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	@Data @NoArgsConstructor @AllArgsConstructor
 	public static class InitMessage implements Serializable {
 		private static final long serialVersionUID = 4057807743872319842L;
-		private SourceRef byteStream;
+		private SourceRef<ByteString> byteStream;
 		private ActorRef sender;
 		private ActorRef receiver;
 	}
@@ -73,7 +73,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	// Actor State //
 	/////////////////´
 	
-	private final ActorMaterializer mat = ActorMaterializer.create(this.getContext());
+	private final Materializer mat = Materializer.createMaterializer(this.getContext());
 	
 	/////////////////////
 	// Actor Lifecycle //
@@ -96,7 +96,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
 		
-		try{
+		
 		Kryo kryo = new Kryo();
 		Output out = new Output(new ByteArrayOutputStream());
         kryo.writeObject(out, message);
@@ -108,19 +108,16 @@ public class LargeMessageProxy extends AbstractLoggingActor {
         Source<ByteString, CompletionStage<IOResult>> source = StreamConverters.fromInputStream(() -> in, CHUNK_SIZE);
         in.close();
         @SuppressWarnings("unchecked")
-		SourceRef<ByteString> sourceRef = (SourceRef<ByteString>) source.runWith(StreamRefs.sourceRef(), mat);
+		SourceRef<ByteString> sourceRef = source.runWith(StreamRefs.sourceRef(), mat);
         
         receiverProxy.tell(new InitMessage(sourceRef, this.sender(), message.getReceiver()), this.self());
         
-		} catch(IOException i) {
-			i.printStackTrace();
-		}	
 	}
 	
 	private void handle(InitMessage message) {
 		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
 		message.getByteStream().getSource().runWith(
-				Sink.foreach(bytes -> System.out.println((string) bytes)),
+				Sink.foreach(bytes -> this.log().info(bytes.toString())),
 				mat);
 	}
 }
